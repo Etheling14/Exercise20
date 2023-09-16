13e9ade777f7bc5f28da07fbafc41656d4afb5d3
Urrea <Surrea1411@gmail.com>
2023-09-16 14:37:55 -0500

    jejeje

---
+++ Exercise20/Program.cs
@@ -0,0 +1,38 @@
+﻿double e = 0;
+
+int quantityPresicionTerms;
+
+double Factorial(double n)
+{
+    if (n == 0)
+    {
+        n = 1;
+        for (double x = n - 1; x > 0; x--)
+        {
+            n = n * x;
+        }
+    }
+    else
+    {
+        for (double x = n - 1; x > 0; x--)
+        {
+            n = n * x;
+        }
+    }
+    return n;
+}
+
+void calculateNumberE(int quantityPresicionTerms)
+{
+    for (int x = 0; x <= quantityPresicionTerms; x++)
+    {
+        e += 1 / Factorial(x);
+    }
+    Console.Write("e = " + e);
+}
+
+Console.Write("How many precision terms you want: ");
+quantityPresicionTerms = int.Parse(Console.ReadLine());
+
+calculateNumberE(quantityPresicionTerms);
+Console.ReadKey();  
